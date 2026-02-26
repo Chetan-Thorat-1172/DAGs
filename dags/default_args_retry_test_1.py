ff5f7db1b6e212c1be47317aea1dda9ac46520a0
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-02-26 18:11:57 +0530

    Update default_args_retry_test_1.py

--- dags/default_args_retry_test_1.py
+++ dags/default_args_retry_test_1.py
@@ -20,8 +20,8 @@ default_args = {
 with DAG(
     dag_id="default_args_retry_test_1",
     default_args=default_args,
-    schedule_interval=None,
-    start_date = datetime(2026,2,24),
+    schedule_interval=@hourly,
+    start_date = datetime(2026,2,27),
     catchup=False,
 ) as dag:
 
