e91b53125159b2e94cad4118d467fd9ba18f49ce
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-02-26 18:13:35 +0530

    Update complex_branch_test_dag.py

--- dags/complex_branch_test_dag.py
+++ dags/complex_branch_test_dag.py
@@ -33,7 +33,7 @@ def task_logic(name):
 
 with DAG(
     dag_id="complex_branch_test_dag",
-    schedule_interval=@daily,
+    schedule_interval="0 * * * *",
     start_date=datetime(2026, 2, 27),
     catchup=False,
 
