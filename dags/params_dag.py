cd7b3c42a269279b0593ced681eb0cf264dc3836
harshvardhan-27afk <harshavardhan.sankpal@pibythree.com>
2026-03-02 16:18:05 +0530

    Update params_dag.py

--- dags/params_dag.py
+++ dags/params_dag.py
@@ -2,7 +2,7 @@ from dag_parser.dynamic.dag_context import DAG
 from dag_parser.dynamic.operators import PythonOperator
 from dag_parser.dynamic.params import Param 
 from datetime import datetime 
-##
+##33
 def show_params(**context):
     dag_run = context["dag_run"]
     params = dag_run.conf or {}
