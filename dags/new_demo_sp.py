533f0336f9833585a60863dc037aa5c6520ffc39
harshvardhan-27afk <harshavardhan.sankpal@pibythree.com>
2026-03-30 12:27:07 +0530

    Change schedule interval to daily

--- dags/new_demo_sp.py
+++ dags/new_demo_sp.py
@@ -5,7 +5,7 @@ from datetime import datetime
 with DAG(
     dag_id="snowflake_etl_dag",
     description="ETL pipeline: Extract → Transform → Load",
-    schedule_interval="0 9 * * *",
+    schedule_interval="@daily",
     start_date=datetime(2026, 3, 30),
     catchup=False,
     tags=["snowflake", "etl"],
