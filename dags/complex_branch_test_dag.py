bd73823efc0a1568e9a3fac4611eff449f64593c
harshvardhan-27afk <harshavardhan.sankpal@pibythree.com>
2026-03-30 13:17:03 +0530

    Change schedule_interval to None in DAG

--- dags/complex_branch_test_dag.py
+++ dags/complex_branch_test_dag.py
@@ -33,7 +33,7 @@ def task_logic(name):
 
 with DAG(
     dag_id="complex_branch_test_dag",
-    schedule_interval="@daily",
+    schedule_interval=None,
     start_date=datetime(2026, 3, 30),
     catchup=False,
 
