04952bebef7eece64d632e15d6832515788a6c08
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-04-24 15:41:27 +0530

    Update dag_6.py

--- dags/dag_6.py
+++ dags/dag_6.py
@@ -13,7 +13,7 @@ def marker(name, **context):
 
 with DAG(
     dag_id="dag_6",
-    schedule_interval="30 15 * * *",
+    schedule_interval="45 15 * * *",
     start_date=datetime(2026, 4, 24),
     catchup=False,
     default_args={"snowflake_conn_id": "harsh_conn", "retries": 1, "retry_delay_seconds": 5},
