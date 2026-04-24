1522ba980e9f91ded474bb1a558448f6e1a7f11e
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-04-24 12:36:01 +0530

    Update dag_4.py

--- dags/dag_4.py
+++ dags/dag_4.py
@@ -26,7 +26,7 @@ def summarize(**context):
 
 with DAG(
     dag_id="dag_4",
-    schedule_interval="15 11 * * *",
+    schedule_interval="45 12 * * *",
     start_date=datetime(2026, 4, 24),
     catchup=False,
     default_args={"snowflake_conn_id": "harsh_conn", "retries": 1, "retry_delay_seconds": 5},
