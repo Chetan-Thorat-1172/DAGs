e75cce66c586e6c6d8a46e2471eb71663fbf683c
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-04-27 17:19:18 +0530

    Update dag_19.py

--- dags/dag_19.py
+++ dags/dag_19.py
@@ -8,8 +8,8 @@ def marker(name, **context):
 
 with DAG(
     dag_id="dag_19",
-    schedule_interval="30 17 * * *",
-    start_date=datetime(2026, 4, 24),
+    schedule_interval="40 17 * * *",
+    start_date=datetime(2026, 4, 27),
     catchup=False,
     default_args={"snowflake_conn_id": "harsh_conn", "retries": 1, "retry_delay_seconds": 5},
     description="Load test DAG 19 with 20 tasks and wide fanout",
