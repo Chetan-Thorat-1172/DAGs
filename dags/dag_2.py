1091aacb985e11f1b9d6393318945c7dff77e149
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-04-27 15:36:36 +0530

    Update schedule interval and start date in DAG 2

--- dags/dag_2.py
+++ dags/dag_2.py
@@ -13,8 +13,8 @@ def marker(name, **context):
 
 with DAG(
     dag_id="dag_2",
-    schedule_interval="30 17 * * *",
-    start_date=datetime(2026, 4, 24),
+    schedule_interval="40 17 * * *",
+    start_date=datetime(2026, 4, 27),
     catchup=False,
     default_args={"snowflake_conn_id": "harsh_conn", "retries": 1, "retry_delay_seconds": 5},
     description="Load test DAG 2 with 16 tasks, branch and notebook/dbt split",
