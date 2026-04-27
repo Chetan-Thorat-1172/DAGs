292b321d39e3e3b32f4387f01326b746cdcf24e7
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-04-27 16:29:22 +0530

    Update dag_4.py

--- dags/dag_4.py
+++ dags/dag_4.py
@@ -26,8 +26,8 @@ def summarize(**context):
 
 with DAG(
     dag_id="dag_4",
-    schedule_interval="25 16 * * *",
-    start_date=datetime(2026, 4, 24),
+    schedule_interval="35 16 * * *",
+    start_date=datetime(2026, 4, 27),
     catchup=False,
     default_args={"snowflake_conn_id": "harsh_conn", "retries": 1, "retry_delay_seconds": 5},
     description="Load test DAG 4 with 18 tasks and param-driven quality branching",
