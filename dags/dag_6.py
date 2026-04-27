6d8934f4061f48180365dfa46629f4ea9a1a6e3d
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-04-27 16:30:07 +0530

    Update dag_6.py

--- dags/dag_6.py
+++ dags/dag_6.py
@@ -13,8 +13,8 @@ def marker(name, **context):
 
 with DAG(
     dag_id="dag_6",
-    schedule_interval="25 16 * * *",
-    start_date=datetime(2026, 4, 24),
+    schedule_interval="35 16 * * *",
+    start_date=datetime(2026, 4, 27),
     catchup=False,
     default_args={"snowflake_conn_id": "harsh_conn", "retries": 1, "retry_delay_seconds": 5},
     description="Load test DAG 6 with 16 tasks and DBT branching",
