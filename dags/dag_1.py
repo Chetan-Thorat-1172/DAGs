b02010dd5273246b2cfbd8b02f5a1a8d4033826b
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-05-06 18:10:03 +0530

    Update dag_1.py

--- dags/dag_1.py
+++ dags/dag_1.py
@@ -3,7 +3,7 @@ from dag_parser.dynamic.dag_context import DAG, SnowflakeOperator
 
 
 with DAG(
-    dag_id="simple_snowflake_dag",
+    dag_id="dag_1",
     schedule_interval="20 18 * * *",  
     start_date=datetime(2026, 5, 6),
     catchup=False,
