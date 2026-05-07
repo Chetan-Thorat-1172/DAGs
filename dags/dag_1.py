aa53eed02072aabdaa82ffe2c8b7f8db81052706
harshvardhan-27afk <harshavardhan.sankpal@pibythree.com>
2026-05-07 17:39:21 +0530

    Change schedule interval for DAG 1

--- dags/dag_1.py
+++ dags/dag_1.py
@@ -13,7 +13,7 @@ def marker(name, **context):
 
 with DAG(
     dag_id="dag_1",
-    schedule_interval="14 16 * * *",
+    schedule_interval="45 17 * * *",
     start_date=datetime(2026, 5, 7),
     catchup=False,
     default_args={"snowflake_conn_id": "harsh_conn", "retries": 1, "retry_delay_seconds": 5},
